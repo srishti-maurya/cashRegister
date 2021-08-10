bf8f9244589f2269743bdb341b6695d5945ba7b9
srishti-maurya <srishti22maurya@gmail.com>
2021-08-10 14:04:36 +0530

    functionality

---
+++ app.js
@@ -0,0 +1,34 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashReceived = document.querySelector("#cash-received");
+const checkButton = document.querySelector("#check-button");
+const message = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+
+const avaiableNotes = [2000, 500, 100, 20, 10, 5, 1];
+
+checkButton.addEventListener("click", function validateBillAndCashAmount() {
+  if (billAmount.value > 0) {
+    if (cashReceived.value >= billAmount.value) {
+      const amountToBeReturned = cashReceived.value - billAmount.value;
+      calculateChange(amountToBeReturned);
+    } else {
+      showMessage(
+        "The cash provided should atleast be equal to the bill amount"
+      );
+    }
+  } else {
+    showMessage("Invalid Bill Amount");
+  }
+});
+
+function calculateChange(amountToBeReturned) {
+  for (let i = 0; i < avaiableNotes.length; i++) {
+    const numberOfNotes = Math.trunc(amountToBeReturned / avaiableNotes[i]);
+    amountToBeReturned %= avaiableNotes[i];
+    noOfNotes[i].innerText = numberOfNotes;
+  }
+}
+
+function showMessage(msg) {
+  message.innerText = "Error: " + msg;
+}
